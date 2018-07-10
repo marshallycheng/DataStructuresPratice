2b83e42b7d6afe037c5aade53668b1f1beaad01c
Marshall <marshallycheng@gmail.com>
2018-07-10 11:21:09 -0700

    Finish graphs

--- Marshall_Cheng_Graphs/lib/install_order.rb
+++ Marshall_Cheng_Graphs/lib/install_order.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 # Given an Array of tuples, where tuple[0] represents a package id,
 # and tuple[1] represents its dependency, determine the order in which
 # the packages should be installed. Only packages that have dependencies
@@ -10,5 +11,40 @@
 
 
 def install_order(arr)
+  pairs = {}
+  queue = []
+  sorted = [] 
+  arr.each do |pair|
+    pairs[pair[0]] = pair[1]
+  end 
+  packages = (1..pairs.keys.max).to_a
+  packages.each do |package|
+    if !pairs.keys.include?(package)
+      queue.push(package)
+    end 
+  end 
 
+  until queue.empty?
+    package = queue.shift 
+    sorted.push(package)
+    neighbors = []
+
+    pairs.each do |key, value|
+      if value == package
+        neighbors.push(key)
+        pairs.delete(key)
+      end 
+    end
+    neighbors.each do |package|
+      if !pairs.keys.include?(package)
+        queue.push(package)
+      end 
+    end 
+  end  
+
+  if packages.length == sorted.length 
+    return sorted
+  else 
+    return []
+  end 
 end
--- Marshall_Cheng_Graphs/lib/topological_sort.rb
+++ Marshall_Cheng_Graphs/lib/topological_sort.rb
@@ -1,7 +1,60 @@
+require 'byebug'
 require_relative 'graph'
 
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
 def topological_sort(vertices)
+  queue = []
+  sorted = []
+  vertices.each do |vertex|
+    if vertex.in_edges.empty?
+      queue.push(vertex)
+    end 
+  end 
 
+  until queue.empty?
+    vertex = queue.shift
+    sorted.push(vertex)
+    neighbors = [] 
+
+    vertex.out_edges.length.times do |i|
+      edge = vertex.out_edges[0]
+      neighbors.push(edge.to_vertex)
+      edge.destroy!
+      # should be enough to remove edge, dont need to explicitly remove vertex
+    end 
+    neighbors.each do |vertex|
+      if vertex.in_edges.empty?
+        queue.push(vertex)
+      end 
+    end 
+  end 
+
+  if vertices.length == sorted.length 
+    return sorted 
+  else 
+    return []
+  end 
 end
+
+# def topological_sort(vertices)
+#   visited = {}
+#   sorted = []
+#   vertices.each do |vertex|
+#     visit(vertex, sorted, visited)
+#     visited[vertex] = true
+#     sorted.unshift(vertex)
+#   end 
+#   sorted
+# end 
+
+# def visit(vertex, sorted, visited)
+#   if visited[vertex] || vertex.out_edges.empty?
+#     sorted.unshift(vertex)
+#   else
+#     vertex.out_edges.each do |edge|
+#       visit(edge.to_vertex, sorted, visited)
+
+#     end 
+#   end 
+# end 
