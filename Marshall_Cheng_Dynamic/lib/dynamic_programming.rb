d8f2791df541099033940bc39e29c5487b689f51
Marshall <marshallycheng@gmail.com>
2018-07-13 16:22:30 -0700

    Finish dynamic

--- Marshall_Cheng_Dynamic/lib/dynamic_programming.rb
+++ Marshall_Cheng_Dynamic/lib/dynamic_programming.rb
@@ -4,6 +4,7 @@ class DynamicProgramming
   def initialize
     # @blair_cache = {1 => 1, 2 => 2}
     @frog_cache = {1 => [[1]], 2=> [[1,1], [2]], 3 => [[1,1,1], [1,2], [2,1], [3]]}
+    @maze_cache = {}
   end
 
   def blair_nums(n)
@@ -85,34 +86,79 @@ class DynamicProgramming
   end
 
   def knapsack(weights, values, capacity)
-    cache = {
-      0 => Array.new(values.length){0}
-    }
-    return cache[capacity][values.length - 1] if capacity < 1
-    (1..capacity).each do |cap|
-      ans = []
-      byebug
-      weights.each_with_index do |weight, idx|
-        if weight > cap && idx == 0
-          ans << cache[cap - 1][idx]
-        elsif weight > cap && idx != 0 
-          ans << ans[idx - 1]
-        elsif cache[cap - weight][idx] + values[idx] >= cache[cap - 1][idx]
-          ans << cache[cap - weight][idx] + values[idx]
-        elsif cache[cap - 1][idx] > cache[cap - weight][idx] + values[idx]
-          ans << cache[cap - 1][idx]
-        end 
-      end 
-      cache[cap] = ans
-    end 
-    cache[capacity][values.length - 1]
+    return 0 if weights.length == 0 || capacity == 0
+    solution_table = knapsack_table(weights, values, capacity)
+    solution_table[capacity][-1]
   end
 
   # Helper method for bottom-up implementation
   def knapsack_table(weights, values, capacity)
+    solution_table = []
 
+    (0..capacity).each do |cap|
+      solution_table[cap] = []
+      (0...weights.length).each do |idx|
+        if capacity == 0 
+          solution_table[cap][idx] = 0
+        elsif idx == 0 
+          solution_table[cap][idx] = weights[idx] > cap ? 0 : values[idx]
+        else
+          option1 = solution_table[cap][idx - 1]
+          option2 = weights[idx] > cap ? 0 : solution_table[cap - weights[idx]][idx - 1] + values[idx]
+          optimum = [option1, option2].max
+          solution_table[cap][idx] = optimum
+        end  
+      end 
+    end 
+    solution_table
   end
 
   def maze_solver(maze, start_pos, end_pos)
+    build_cache(start_pos)
+    solve_maze(maze, start_pos, end_pos)
+    find_path(end_pos)
   end
+
+  def solve_maze(maze, start_pos, end_pos)
+    return true if start_pos == end_pos 
+
+    get_moves(maze, start_pos).each do |new_spot|
+      unless @maze_cache.keys.include?(new_spot)
+        @maze_cache[new_spot] = start_pos
+        solve_maze(maze, new_spot, end_pos)
+      end 
+    end 
+  end 
+
+
+  def find_path(end_pos)
+    path = []
+    current = end_pos
+    until current.nil?
+      path.unshift(current)
+      current = @maze_cache[current]
+    end 
+
+    path
+  end 
+  def build_cache(start_pos)
+    @maze_cache[start_pos] = nil
+  end 
+
+  def get_moves(moves, from_pos)
+    directions = [[0,1],[1,0], [-1,0], [0,-1]]
+    x, y = from_pos 
+    result = []
+
+    directions.each do |dx, dy|
+      new_spot = [x + dx, y + dy]
+      result << new_spot if is_valid_pos?(maze, new_spot)
+    end 
+    result
+  end
+
+  def is_valid_pos?(maze, pos)
+    x, y = pos
+    x >= 0 && y >= 0 && x < maze.length && y < maze.first.length && maze[x][y]
+  end 
 end
