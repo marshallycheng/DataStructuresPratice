ae37984104af08568937c0f6f9078bb90bfb7764
Marshall <marshallycheng@gmail.com>
2018-08-15 19:25:34 -0700

    Add preorder traversal

--- tree_traversal.rb
+++ tree_traversal.rb
@@ -6,4 +6,20 @@ class Node
     @left = nil 
     @right = nil
   end  
+
+  def preorder(node)
+    return unless node
+    p node.val 
+    preorder(node.left)
+    preorder(node.right)
+  end 
+
+  def preorder_array(node)
+    return [] unless node 
+    results = []
+    results.push(node.val)
+    results.concat(preorder_array(node.left))
+    results.concat(preorder_array(node.right))
+    results
+  end 
 end 
